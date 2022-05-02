cdbb87b3a5cdea1a069b55530a2b236c5a2e04fe
AnastasiyaRuslanovna <104769848+AnastasiyaRuslanovna@users.noreply.github.com>
2022-05-02 15:36:06 +0300

    Add files via upload
    Научили приложение здороваться с нами

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zadanie_1._5._1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, {0}", name);
+        }
+    }
+}
