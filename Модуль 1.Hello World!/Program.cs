8c6a260a2e9507a18096c6ca993694439faff195
PolyanskayaAR <asya150203@gmail.com>
2022-05-02 15:29:58 +0300

    Добавьте файлы проекта.

---
+++ Модуль 1.Hello World!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Модуль_1.Hello_World_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
